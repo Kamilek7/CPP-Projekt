c9f838ede1fa76be13d4d08aeb04844f2326d85b
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2025-01-18 18:00:07 +0100

    aasdasd

--- OpenGL/animation.h
+++ OpenGL/animation.h
@@ -75,12 +75,89 @@ public:
 		this->empty = false;
 	}
 
+	glm::vec3 getInterpolatedScale(float time, int boneID)
+	{
+		float timeInTicks = time * ticksPerSecond;
+		if (looped)
+		{
+			timeInTicks = fmod(timeInTicks, duration);
+		}
+		if (animationPackets[boneID].s.size() == 1)
+		{
+			return animationPackets[boneID].s[0].scale;
+		}
+		else if (animationPackets[boneID].s.size() > 1)
+		{
+			int id = 0;
 
-	glm::vec3 getInterpolatedScale(float time, int boneID);
+			for (int i = 0; i < animationPackets[boneID].s.size() - 1; i++) {
+				if (timeInTicks < animationPackets[boneID].s[i + 1].t) {
+					id = i;
+					break;
+				}
+			}
 
-	glm::vec3 getInterpolatedPosition(float time, int boneID);
+			// No interpolation is actually needed when using blender
 
-	aiQuaternion getInterpolatedRotation(float time, int boneID);
+			glm::vec3 s1 = animationPackets[boneID].s[id].scale;
+
+			return s1;
+		}
+	}
+
+	glm::vec3 getInterpolatedPosition(float time, int boneID)
+	{
+		float timeInTicks = time * ticksPerSecond;
+		if (looped)
+		{
+			timeInTicks = fmod(timeInTicks, duration);
+		}
+		if (animationPackets[boneID].t.size() == 1)
+		{
+			return animationPackets[boneID].t[0].translation;
+		}
+		else if (animationPackets[boneID].t.size() > 1)
+		{
+			int id = 0;
+			for (int i = 0; i < animationPackets[boneID].t.size() - 1; i++) {
+				if (timeInTicks < animationPackets[boneID].t[i + 1].t) {
+					id = i;
+					break;
+				}
+			}
+
+			glm::vec3 s1 = animationPackets[boneID].t[id].translation;
+
+			return s1;
+		}
+	}
+
+	aiQuaternion getInterpolatedRotation(float time, int boneID)
+	{
+		float timeInTicks = time * ticksPerSecond;
+		if (looped)
+		{
+			timeInTicks = fmod(timeInTicks, duration);
+		}
+		if (animationPackets[boneID].r.size() == 1)
+		{
+			return animationPackets[boneID].r[0].rotation;
+		}
+		else if (animationPackets[boneID].r.size() > 1)
+		{
+			int id = 0;
+			for (int i = 0; i < animationPackets[boneID].r.size() - 1; i++) {
+				if (timeInTicks < animationPackets[boneID].r[i + 1].t) {
+					id = i;
+					break;
+				}
+			}
+
+			aiQuaternion Out = animationPackets[boneID].r[id].rotation;
+
+			return Out;
+		}
+	}
 
 };
 
